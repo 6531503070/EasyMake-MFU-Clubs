e1c6a82d54b16cbf56c46a663351a398281f172f
6531503070 <6531503070@lamduan.mfu.ac.th>
2025-10-29 23:24:11 +0700

    tests: Update login tests for admin dashboard with new email and password formats
    - Modified invalid login test to use a more specific error message.
    - Updated valid login test with new email and password values.
    - Enhanced assertions to verify the presence of relevant UI elements after login attempts.

--- tests/login-admin-dashboard-invalid.spec.ts
+++ tests/login-admin-dashboard-invalid.spec.ts
@@ -2,18 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('login-admin-dashboard-invalid', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  
-  await page.getByRole('link', { name: 'Admin Dashboard' }).click();
-  await expect(page.locator('body')).toContainText('EasyMake • MFU Clubs');
-  await expect(page.getByRole('heading')).toContainText('Admin Sign In');
 
+  await page.getByRole('link', { name: 'Admin Dashboard' }).click();
   await page.getByRole('textbox', { name: 'you@mfu.ac.th' }).click();
-  await page.getByRole('textbox', { name: 'you@mfu.ac.th' }).fill('abc1234');
+  await page.getByRole('textbox', { name: 'you@mfu.ac.th' }).fill('abc1234@gmail.com');
   await page.getByRole('textbox', { name: '••••••••••' }).click();
   await page.getByRole('textbox', { name: '••••••••••' }).fill('1234');
   await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.getByRole('textbox', { name: 'you@mfu.ac.th' }).click();
-  await page.getByRole('textbox', { name: 'you@mfu.ac.th' }).fill('abc1234@gmail.com');
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await expect(page.locator('body')).toContainText('Failed to fetch');
+  
+  await expect(page.locator('body')).toContainText('Cannot read properties of undefined (reading \'user\')');
+  await expect(page.getByRole('heading')).toContainText('Admin Sign In');
+  await expect(page.locator('body')).toContainText('Club Leaders and University Staff only.');
 });
